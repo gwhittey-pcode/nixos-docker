bb7df3a27e1c7052be7f0d05960524b3fcef8195
Gerard Whittey <gerard.whittey@gmail.com>
2025-02-07 00:14:21 -0500

    added flatpak

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -18,6 +18,8 @@
 
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
+    #flatpak handaling
+    ./flatpak/flatpak.nix
   ];
 
   nixpkgs = {
